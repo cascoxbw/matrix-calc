1f746363b788339f3499653e27976caf24a542a9
bowenxu <bowen.xu@intel.com>
2022-08-26 10:28:44 +0800

    add kron

--- coma.cpp
+++ coma.cpp
@@ -92,8 +92,8 @@ void calc_coma_avx512_float(int32_t caseid, int32_t N)
     int32_t len = N*N;
     float *out_re = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
     float *out_im = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
-    float *in_re  = (float *)calloc(MAX_NUM_COMA*2, sizeof(float));
-    float *in_im  = (float *)calloc(MAX_NUM_COMA*2, sizeof(float));
+    float *in_re  = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
+    float *in_im  = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
 
     for (int32_t i=0;i<NUM_LOOP;i++)
     {
@@ -115,8 +115,8 @@ void calc_coma_avx512_double(int32_t caseid, int32_t N)
     int32_t len = N*N;
     double *out_re = (double *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(double));
     double *out_im = (double *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(double));
-    double *in_re  = (double *)calloc(MAX_NUM_COMA*2, sizeof(double));
-    double *in_im  = (double *)calloc(MAX_NUM_COMA*2, sizeof(double));
+    double *in_re  = (double *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(double));
+    double *in_im  = (double *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(double));
 
     for (int32_t i=0;i<NUM_LOOP;i++)
     {
@@ -131,6 +131,107 @@ void calc_coma_avx512_double(int32_t caseid, int32_t N)
     free(out_im);
     free(in_re);
     free(in_im);
+}
+
+void vec_mul512(float* m_re, float* m_im, float* n_re, float* n_im, int32_t len, float* out_re, float* out_im)
+{
+    __m512 Re,Im;
+    int32_t N_loop = len/DATA512Float_LOOP;
+    float re_temp = 0;
+    float im_temp = 0;
+    __m512 Re_sum[128],Im_sum[128];
+    // Re_sum = _mm512_setzero_ps();
+    // Im_sum= _mm512_setzero_ps();
+    for(int32_t loop_index = 0; loop_index < N_loop;loop_index++)
+    {
+        __m512 m_re_512 = _mm512_load_ps(m_re+loop_index*DATA512Float_LOOP);
+        __m512 n_re_512 = _mm512_load_ps(n_re+loop_index*DATA512Float_LOOP);
+        __m512 m_im_512 = _mm512_load_ps(m_im+loop_index*DATA512Float_LOOP);
+        __m512 n_im_512 = _mm512_load_ps(n_im+loop_index*DATA512Float_LOOP);
+        *(Re_sum+loop_index) = _mm512_sub_ps(_mm512_mul_ps(m_re_512,n_re_512),_mm512_mul_ps(m_im_512,n_im_512));
+        *(Im_sum+loop_index) = _mm512_add_ps(_mm512_mul_ps(m_re_512,n_im_512),_mm512_mul_ps(m_im_512,n_re_512));
+        // Re_sum = _mm512_add_ps(Re,Re_sum);
+        // Im_sum = _mm512_add_ps(Im,Im_sum);
+    }
+    Re = _mm512_setzero_ps();
+    Im = _mm512_setzero_ps();
+    for(int32_t loop_index = 0; loop_index < N_loop; loop_index++)
+    {
+        Re = _mm512_add_ps(Re_sum[loop_index],Re);
+        Im = _mm512_add_ps(Re_sum[loop_index],Im);
+    }
+    *out_re = _mm512_reduce_add_ps(Re);
+    *out_im = _mm512_reduce_add_ps(Im);
+}
+
+void matrix_mul512(float* m_re, float* m_im, float* n_re, float* n_im, int32_t m_row, int32_t m_col, int32_t n_row, int32_t n_col, float* out_re, float* out_im)
+{
+    if(m_col!=n_row)
+    {
+        printf("error matrix multiplexing dimentions\n");
+    }
+    float* m_re_start = NULL;
+    float* n_re_start = NULL;
+    float* m_im_start = NULL;
+    float* n_im_start = NULL;
+    // the second matrix col
+    for(int32_t n_col_index = 0; n_col_index < n_col; n_col_index ++)
+    {
+        n_re_start = n_re + n_row*n_col_index;
+        n_im_start = n_im + n_row*n_col_index;
+        for(int32_t m_row_index = 0; m_row_index < m_row; m_row_index ++)
+        {
+            m_re_start = m_re + m_col*m_row_index;
+            m_im_start = m_im + m_col*m_row_index;
+            //printf("m_re %f,m_im %f,n_re %f, n_im %f\n",*m_re_start,*m_im_start,*n_re_start,*n_im_start);
+            vec_mul512(m_re_start,m_im_start,n_re_start,n_im_start,m_row,out_re+m_row_index*n_col+n_col_index,out_im+m_row_index*n_col+n_col_index);
+           // printf("n_col_index is %d,m_col_index is %d,re is %f,im is %f\n",n_col_index,m_row_index,*out_re,*out_im);
+        }
+    }
+}
+
+void calc_kron_avx512_float(int32_t caseid, int32_t N)
+{   
+    int32_t len = N*N;
+    float *out_re = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
+    float *out_im = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
+    float *in_re1  = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
+    float *in_im1  = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
+    float *in_re2  = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
+    float *in_im2  = (float *)calloc(MAX_NUM_COMA*MAX_NUM_COMA*2, sizeof(float));
+
+    for (int32_t i=0;i<NUM_LOOP;i++)
+    {
+        uint64_t t1 = __rdtsc();
+        if (N == 4)
+        {
+            for(int32_t m_row_index = 0; m_row_index < N; m_row_index ++)
+            {
+                for(int32_t n_col_index = 0; n_col_index < len*2; n_col_index ++)
+                {
+                    for(int32_t m_col_index = 0; m_col_index < N; m_col_index ++)
+                    {
+                        *(out_re+n_col_index+m_row_index*len*2)+=*(in_re1+m_col_index+N*m_row_index)*(*(in_re2+n_col_index+len*2*m_col_index));
+                        *(out_im+n_col_index+m_row_index*len*2)+=*(in_im1+m_col_index+N*m_row_index)*(*(in_im2+n_col_index+len*2*m_col_index));
+                    }
+                }
+            }
+        }
+        else
+        {
+            matrix_mul512(in_re1,in_im1,in_re2,in_im2,N,N,N,len*2,out_re,out_im);
+        }
+        uint64_t t2 = __rdtsc();
+        gCycleCount[caseid][i] = t2-t1;
+    }
+    printf(" case %d: calc_kron_%d_avx512_float , end \n", caseid, N);
+
+    free(out_re);
+    free(out_im);
+    free(in_re1);
+    free(in_im1);
+    free(in_re2);
+    free(in_im2);
 }
 
 int main(int argc, char *argv[])
@@ -151,8 +252,10 @@ int main(int argc, char *argv[])
     calc_coma_avx512_double(6, 32);
     calc_coma_avx512_double(7, 64);
 
-
-
+    calc_kron_avx512_float(8, 4);
+    calc_kron_avx512_float(9, 8);
+    calc_kron_avx512_float(10, 32);
+    calc_kron_avx512_float(11, 64);
     
     printf("****************************\n");
     printf(" test end \n");
